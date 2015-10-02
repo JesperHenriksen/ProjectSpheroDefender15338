aa74b947c18f7f07b5c00458afc86301861245df
JesperWH <jesper.waedeled.henriksen@gmail.com>
2015-10-02 10:39:19 +0200

    Changes to cameraFeed

--- SpheroDefender15338/SpheroDefender15338/CameraFeed.h
+++ SpheroDefender15338/SpheroDefender15338/CameraFeed.h
@@ -6,8 +6,51 @@ class CameraFeed
 public:
 	CameraFeed();
 	~CameraFeed();
-	Mat getImageFromWebcam(VideoCapture capture);
-	Mat negateChannel(int channelNegate1, Mat frame);
+	Mat convertRGBtoGS(Mat inputFrame);
+	Mat segmentImage(Mat inputFrame);
+	/**
+	@brief
+		Thresholds an Mat image based on a min value, max value with a new value.
+	@param inputImage
+		This is the input Mat image.
+	@param outputImage
+		This is the output Mat image.
+	@param minThreshold
+		Minimum value which the threshold should evaluate.
+	@param maxThreshold
+		Maximum value which the threshold should evaluate.
+	@param newValue
+		If pixel matches the threshold then replace the pixel value with the new value.		
+	*/
+	void thresholdImage(Mat inputImage, Mat outputImage, int minThreshold, int maxThreshold, int newValue);
+	/**
+	 @brief 
+		Returns an image from the webcam.
+	 @param capture
+		The webcam you want a picture from.
+	 @return
+		An image taken with the webcam.
+	*/
+	Mat getImageFromWebcam(int capture);
+	/**
+	 @brief
+		Negates a given channel of a Mat
+	 @param channelNegate
+		The channel which should be negated
+	 @param frame
+		The input Mat frame.
+	 @return
+		Returns an image with a given channel negated 
+	*/
+	Mat negateChannel(int channelNegate, Mat frame);
+private:
+	/**
+	@brief
+		Used by negateChannel to set zeroes in the given channel
+	@param inputFrame
+		Input Mat frame which the channel is negated.
+	*/
 	Mat setZeroesInChannel(Mat inputFrame);
+
 };
 
