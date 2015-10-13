45fcc557f40d6e12046f9d3ae61c4b7226794020
rtoft <rtoft14@student.aau.dk>
2015-10-13 13:57:18 +0200

    Placement of blob in minimap

--- SpheroDefender15338/SpheroDefender15338/main.cpp
+++ SpheroDefender15338/SpheroDefender15338/main.cpp
@@ -9,14 +9,20 @@ using namespace std;
 
 int main(int, char)
 {
-	CameraFeed camfeed; 
+	CameraFeed camfeed(1); 
+	Minimap minimap;
+	for (;;){
+		Mat frame;
+		frame = minimap.segmentImage(camfeed);
+		imshow("test", frame);
+		if (waitKey(30) >= 0)
+			break;
+	}
 	//Mat frame;
 	//frame = camfeed.getImageFromWebcam(0);
 	//frame = camfeed.convertRGBtoGS(frame);
 	//frame = camfeed.segmentImage(frame);
 	//imshow("test", frame);
-	Minimap hej;
-	hej.segmentImage();
 	waitKey(0);
 	return 0;
 }
