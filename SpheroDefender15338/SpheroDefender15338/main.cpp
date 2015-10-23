ab0811d088fc9b7a77aad8630d1fa74ad0266d37
Andy <arizonakid92@gmail.com>
2015-10-23 12:29:46 +0200

    Andrew changes

--- SpheroDefender15338/SpheroDefender15338/main.cpp
+++ SpheroDefender15338/SpheroDefender15338/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main(int, char)
 {
 	CameraFeed standardWebcam(0); 
-	CameraFeed webcamOne(1);
+	CameraFeed webcamOne(0);
 	Mat frame, raw, blob, gs;
 	Minimap minimap;
 	//for (;;){
@@ -32,32 +32,37 @@ int main(int, char)
 	//}
 	
 	
-	/*for (;;){//uncomment for coordinates (placeSpell)
-		Mat frame = webcamOne.getImageFromWebcam(), gs;
-		double x = 0, y = 0;
-		frame = webcamOne.convertRGBtoGS(frame);
-		frame.copyTo(gs);
-		standardWebcam.thresholdImage(frame,frame,100,255,0,0,100,255);
-		minimap.placeSpell(frame);
-		imshow(" ", frame);
-		imshow(".", gs);
-		if (waitKey(30) >= 0)
-			break;
-	}*/
+	//for (;;){//uncomment for coordinates (placeSpell)
+	//	Mat frame = webcamOne.getImageFromWebcam(), gs;
+	//	double x = 0, y = 0;
+	//	frame = webcamOne.convertRGBtoGS(frame);
+	//	frame.copyTo(gs);
+	//	standardWebcam.thresholdImage(frame,frame,100,255,0,0,100,255);
+	//	minimap.placeSpell(frame);
+	//	imshow(" ", frame);
+	//	imshow(".", gs);
+	//	if (waitKey(30) >= 0)
+	//		break;
+	//}
 
-	/*for (;;){//uncommet if you want the angle
-		Mat frame = webcamOne.getImageFromWebcam(), gs;
-		frame = standardWebcam.convertRGBtoGS(frame);
+	for (;;){//uncommet if you want the angle
+		Mat frame = webcamOne.getImageFromWebcam(), gs, thresholded;
+		//webcamOne.thresholdImageColor(frame, frame, 100, 160, 150, 0, 255, 0, 0, 255, 0);
+		minimap.thresholdImageArrow(frame, frame, 100, 160, 255, 0, 70, 0, 70);
 		frame.copyTo(gs);
-		standardWebcam.thresholdImage(frame,frame,100,255,0,40,100,150,0,40,255); 
+		gs = webcamOne.convertRGBtoGS(frame);
+		medianBlur(gs, gs, 5);
+		gs.copyTo(thresholded);
+		webcamOne.thresholdImage(thresholded, thresholded, 49, 255, 0, 0, 49, 150);
 		double angle = 0; 
-		angle = standardWebcam.getAngleOfVector(frame,150,255);
-		cout << angle << " ";
-		imshow(" ", frame);
-		imshow(".", gs);
+		angle = webcamOne.getAngleOfVector(gs, 150, 255);
+		cout << angle << " " << "\n";
+		imshow("color", frame);
+		imshow("grayscale", gs);
+		imshow("threshold", thresholded);
 		if (waitKey(30) >= 0)
 			break;
-	}*/
+	}
 	waitKey(0);
 	return 0;
 }
