543f23a6d55749c66994fc769b2e5e89a4014ad4
Ounwei <ecdz12@student.aau.dk>
2015-10-26 10:20:24 +0100

    Added comments to SubtractBackground

--- SpheroDefender15338/SpheroDefender15338/BackgroundSubtraction.cpp
+++ SpheroDefender15338/SpheroDefender15338/BackgroundSubtraction.cpp
@@ -1,15 +1,13 @@
 #include "BackgroundSubtraction.h"
-#include <queue>
-#include <list>
 
 using namespace cv;
 using namespace std;
 
-vector<Mat> model;
-Mat collectedBackground;
-Mat clearBackground;
-Mat mask;
-int frameLimit = 300; //Maximum number of frames stored in queue
+vector<Mat> model; //Mat for storing the frame captured by the camera
+Mat collectedBackground; //Collects the model Mat
+//Mat clearBackground; //Assigned to collectedBackground to avoid getting a total black picture
+Mat mask; //Mat for holding the mask
+int frameLimit = 300; //Maximum number frames allowed to be stored
 
 
 BackgroundSubtraction::BackgroundSubtraction()
@@ -20,38 +18,41 @@ BackgroundSubtraction::~BackgroundSubtraction()
 {
 }
 
+//Function for getting the background
+Mat BackgroundSubtraction::subtractBackground(Mat frame) {
+    if (model.size() >= frameLimit) { //if the model's size is bigger or equal to the frame limit
+        subtract(frame, mask, frame); //subtract the mask from the frame and output it into the frame
+    }
+    else {
+        averageBackground(frame); //Calls the function averageBackground
+    }
+    return frame; //Returns the value of frame
+}
 
+//Function for getting the average background
 void BackgroundSubtraction::averageBackground(Mat frame) {
     Mat grayFrame;
     //frame.copyTo(grayFrame);
-    frame.convertTo(grayFrame, CV_8UC1);
-    frame.convertTo(grayFrame, CV_16UC1);
+    frame.convertTo(grayFrame, CV_8UC1); //Apply 8 bit to grayFrame
+    frame.convertTo(grayFrame, CV_16UC1); //Apply 16 bit to grayFrame to avoid clipping
 
-    model.push_back(grayFrame);
+    model.push_back(grayFrame); //Push grayFrame to the back of model
 
+    //Loop through the size of model and add the position of the model at i and the collectedBackground to the collectedBackground
     for (int i = 1; i < model.size(); i++) {
         add(model.at(i), collectedBackground, collectedBackground);
     }
 
-    createMask();
-        
+    createMask(); //Call function for creating the mask
+    
+    //Checking if the size of model is bigger than the frame limit and if so the element in the beggining of model is erased
     if (model.size() >= frameLimit) {
         model.erase(model.begin());
     }
 }
 
+//Function for making a mask
 void BackgroundSubtraction::createMask() {
-    convertScaleAbs(collectedBackground, mask, 1.0 / model.size());
-    mask.convertTo(mask, CV_8UC1);
-}
-
-Mat BackgroundSubtraction::subtractBackground(Mat frame) {
-    
-    if (model.size() >= frameLimit) {
-        subtract(frame, mask, frame); 
-    }
-    else {
-        averageBackground(frame);
-    }
-    return frame;
+    convertScaleAbs(collectedBackground, mask, 1.0 / model.size()); //Calculates the absolute values taking in collectedBackground, outputting mask and using the scale factor 1.0 divided with the size of the model
+    mask.convertTo(mask, CV_8UC1); //Converts the mask to 8 bit
 }
