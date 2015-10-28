f7076577e6467a29a34063d5e28da7885c464419
rtoft <rtoft14@student.aau.dk>
2015-10-28 13:19:09 +0100

    main

--- SpheroDefender15338/SpheroDefender15338/main.cpp
+++ SpheroDefender15338/SpheroDefender15338/main.cpp
@@ -2,6 +2,7 @@
 #include "Sphero.h"
 #include "Minimap.h"
 #include "CameraFeed.h"
+#include "UserInterface.h"
 #include <thread>
 
 using namespace cv;
@@ -9,21 +10,23 @@ using namespace std;
 
 int main(int, char)
 {
-	CameraFeed standardWebcam(0); 
+	CameraFeed standardWebcam(1); 
 	Minimap minimap;
-	for (;;){
-		Mat frame, raw, blob;
-		raw = standardWebcam.getImageFromWebcam();
-		frame = standardWebcam.getImageFromWebcam();
-		frame = standardWebcam.convertRGBtoGS(frame);
-		standardWebcam.thresholdImage(frame, frame, 0, 100, 0);
-		standardWebcam.thresholdImage(frame, frame, 100, 230, 255);
-		standardWebcam.thresholdImage(frame,frame, 230,255,0);
-		imshow("test", frame);
-		blob = standardWebcam.grassFire(frame);
-		imshow("raw input",blob);
-		if (waitKey(30) >= 0)
-			break;
-	}
+	UserInterface userInterface;
+	userInterface.loadSpells();
+	//for (;;){
+	//	Mat frame, raw, blob;
+	//	raw = standardWebcam.getImageFromWebcam();
+	//	frame = standardWebcam.getImageFromWebcam();
+	//	frame = standardWebcam.convertRGBtoGS(frame);
+	//	standardWebcam.thresholdImage(frame, frame, 0, 100, 0);
+	//	standardWebcam.thresholdImage(frame, frame, 100, 230, 255);
+	//	standardWebcam.thresholdImage(frame,frame, 230,255,0);
+	//	imshow("test", frame);
+	//	blob = standardWebcam.grassFire(frame);
+	//	imshow("raw input",blob);
+	//	if (waitKey(30) >= 0)
+	//		break;
+	//}
 	return 0;
 }
