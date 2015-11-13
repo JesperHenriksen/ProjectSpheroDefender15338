03eb910a6f001082ee5bbee62e8cd6b59ff652cd
rtoft <rtoft14@student.aau.dk>
2015-11-13 09:51:11 +0100

    rebecca commit

--- SpheroDefender15338/SpheroDefender15338/main.cpp
+++ SpheroDefender15338/SpheroDefender15338/main.cpp
@@ -21,57 +21,62 @@ int main(int, char)
     Mat foreground;
 	Minimap minimap;
 	UserInterface userInterface;
-	userInterface.interfaceLayers();
-	imshow("left menu", userInterface.getMenu());
-	
-	for (;;){
-		//arrow position variables
-		double x = 0, y = 0;
-		double minimapXCoord = 0, minimapYCoord = 0;
-		Mat frame, gs;
-		// get coordinates for arrow
-		frame = webcamOne.getImageFromWebcam();
-		frame = webcamOne.convertRGBtoGS(frame);
-		frame.copyTo(gs);
-		standardWebcam.thresholdImage(frame,frame,100,255,0,0,100,255);
-		//minimap.placeSpell(frame, 50,255,minimapXCoord,minimapYCoord);
+	double x = 0.0, y = 0.0;
 
-		//angle of arrow variables
-		double angle = 0;
-		Mat angleInput, thresholded, angleGrayscale;
-		//get the angle of arrow
-		angleInput = webcamOne.getImageFromWebcam();
-		angleInput *= 1.5;
-		//imshow("raw", angleInput);
-		medianBlur(angleInput, angleInput, 5);
-		webcamOne.thresholdImageColor(angleInput, angleInput, 80, 255, 255, 100, 255, 255, 100, 255, 255);
-		webcamOne.thresholdImageColor(angleInput, angleInput, 0, 80, 0, 0, 100, 0, 0, 100, 0);
-		angleGrayscale = webcamOne.convertRGBtoGS(angleInput);
-		angleGrayscale = angleGrayscale * 1.5;
-		medianBlur(angleGrayscale, angleGrayscale, 7);
-		angleGrayscale *= 2;
-		angleGrayscale.copyTo(thresholded);
-		webcamOne.thresholdImage(thresholded, thresholded, 150, 255, 255, 60, 150, 100, 0, 60, 0);
-		//imshow("threshold", thresholded);
-		angle = minimap.getAngleOfArrow(thresholded, 0, 100);
-		//cout << angle << " " << "\n";
-	
-		//hand tracking variables
-		Mat handInput, handColorThreshold, handGscale, handThreshold;
-		//hand thresholding
-		handInput = standardWebcam.getImageFromWebcam();
-		handInput *= 2;
-		handInput.copyTo(handColorThreshold);
-		webcamOne.thresholdImageColor(handInput, handColorThreshold, 100, 255, 0, 140, 200, 255, 100, 255, 0);
-		webcamOne.thresholdImageColor(handColorThreshold, handColorThreshold, 0, 100, 0, 0, 140, 0, 0, 100, 0);
+	userInterface.getStartPoint(wizardBackground, x, y);
+	cout << x << ", " << y;
 
-
-
-		//end of code
-		if (waitKey(30) >= 0)
-			break;
-	}
 	waitKey(0);
+
+	
+	//for (;;){
+	//	//arrow position variables
+	//	double x = 0, y = 0;
+	//	double minimapXCoord = 0, minimapYCoord = 0;
+	//	Mat frame, gs;
+	//	// get coordinates for arrow
+	//	frame = webcamOne.getImageFromWebcam();
+	//	frame = webcamOne.convertRGBtoGS(frame);
+	//	frame.copyTo(gs);
+	//	standardWebcam.thresholdImage(frame,frame,100,255,0,0,100,255);
+	//	//minimap.placeSpell(frame, 50,255,minimapXCoord,minimapYCoord);
+
+	//	//angle of arrow variables
+	//	double angle = 0;
+	//	Mat angleInput, thresholded, angleGrayscale;
+	//	//get the angle of arrow
+	//	angleInput = webcamOne.getImageFromWebcam();
+	//	angleInput *= 1.5;
+	//	//imshow("raw", angleInput);
+	//	medianBlur(angleInput, angleInput, 5);
+	//	webcamOne.thresholdImageColor(angleInput, angleInput, 80, 255, 255, 100, 255, 255, 100, 255, 255);
+	//	webcamOne.thresholdImageColor(angleInput, angleInput, 0, 80, 0, 0, 100, 0, 0, 100, 0);
+	//	angleGrayscale = webcamOne.convertRGBtoGS(angleInput);
+	//	angleGrayscale = angleGrayscale * 1.5;
+	//	medianBlur(angleGrayscale, angleGrayscale, 7);
+	//	angleGrayscale *= 2;
+	//	angleGrayscale.copyTo(thresholded);
+	//	webcamOne.thresholdImage(thresholded, thresholded, 150, 255, 255, 60, 150, 100, 0, 60, 0);
+	//	//imshow("threshold", thresholded);
+	//	angle = minimap.getAngleOfArrow(thresholded, 0, 100);
+	//	//cout << angle << " " << "\n";
+	//
+	//	//hand tracking variables
+	//	Mat handInput, handColorThreshold, handGscale, handThreshold;
+	//	//hand thresholding
+	//	handInput = standardWebcam.getImageFromWebcam();
+	//	handInput *= 2;
+	//	handInput.copyTo(handColorThreshold);
+	//	webcamOne.thresholdImageColor(handInput, handColorThreshold, 100, 255, 0, 140, 200, 255, 100, 255, 0);
+	//	webcamOne.thresholdImageColor(handColorThreshold, handColorThreshold, 0, 100, 0, 0, 140, 0, 0, 100, 0);
+
+
+
+	//	//end of code
+	//	if (waitKey(30) >= 0)
+	//		break;
+	//}
+	//waitKey(0);
 	
 	//wizardBackground = webcamOne.convertRGBtoGS(wizardBackground);
 
